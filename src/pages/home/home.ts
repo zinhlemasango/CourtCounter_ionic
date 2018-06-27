d6edf0bc0fccbe53320615236e1c97c2b172c8a5
Zinhle <mcravemap@gmail.com>
2018-06-27 10:46:13 +0200

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,52 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  value = 0;
+  value2 = 0;
+ text = "";
+  
+
   constructor(public navCtrl: NavController) {
 
   }
 
+click3(){
+this.value +=3;
 }
+click2(){
+  this.value +=2;
+}
+
+click1(){
+  this.value +=1
+  
+}
+
+click4(){
+  this.value2 +=3;
+  }
+  click5(){
+    this.value2 +=2;
+  }
+  
+  click6(){
+    this.value2 +=1
+    
+  }
+
+  clickReset(){
+    this.value = 0;
+    this.value2 = 0;
+
+  }
+
+
+  
+  }
+  
+  
+
+
+
+
+
